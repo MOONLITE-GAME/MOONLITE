568b369a676a75ee05761aa029abf9a55a3cc5d4
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-03-30 14:43:31 -0400

    audio engine

--- source/scenes/menus/titleScreen.gd
+++ source/scenes/menus/titleScreen.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	AudioEngine.loadSong("titleScreen");
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("uiSELECT"):
 		get_tree().change_scene_to_file("res://source/scenes/menus/mainMenu.tscn");
