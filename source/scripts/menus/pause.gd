9dcec573f1c6dc722d00599f9f057aa4fca75912
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-03-31 20:08:08 -0400

    pause menu kinda finished a little bit i guess

--- source/scripts/menus/pause.gd
+++ source/scripts/menus/pause.gd
@@ -4,6 +4,40 @@ var curButton:int = 1;
 
 func _process(_delta: float) -> void:
 	checkPauseInput();
+	
+	if get_tree().paused:
+		if Input.is_action_just_pressed("uiUP"):
+			$uiSounds.play();
+			match curButton:
+				1:
+					$resume.play_backwards("hover");
+					$mainMenu.play("hover");
+					curButton = 2;
+				2:
+					$mainMenu.play_backwards("hover");
+					$resume.play("hover");
+					curButton = 1;
+		if Input.is_action_just_pressed("uiDOWN"):
+			$uiSounds.play();
+			match curButton:
+				1:
+					$resume.play_backwards("hover");
+					$mainMenu.play("hover");
+					curButton = 2;
+				2:
+					$mainMenu.play_backwards("hover");
+					$resume.play("hover");
+					curButton = 1;
+		
+		if Input.is_action_just_pressed("uiSELECT"):
+			match curButton:
+				1:
+					if get_tree().paused:
+						resume();
+				2:
+					if get_tree().paused:
+						resume();
+						get_tree().change_scene_to_file("res://source/scenes/menus/titleScreen.tscn");
 
 func checkPauseInput():
 	if Input.is_action_just_pressed("Pause") && get_tree().paused == false:
