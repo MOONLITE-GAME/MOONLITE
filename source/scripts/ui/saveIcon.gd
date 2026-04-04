7040be21181e31200f13a79266b898f1eae5e7f3
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-04 13:10:31 -0400

    smooth fade out

--- source/scripts/ui/saveIcon.gd
+++ source/scripts/ui/saveIcon.gd
@@ -4,8 +4,9 @@ func _ready() -> void:
 	visible = false;
 
 func showIcon():
+	$AnimationPlayer.play("RESET");
 	visible = true;
 	$icon.play("loading blob");
 	await get_tree().create_timer(1.0).timeout;
+	$AnimationPlayer.play("fadeOut");
 	$icon.stop();
-	visible = false;
