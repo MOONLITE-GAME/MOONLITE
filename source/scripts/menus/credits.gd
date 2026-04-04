bce8f161ff49b7081f6a43a2dfae97c6b9bb3015
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-03 21:30:05 -0400

    credits music and shi

--- source/scripts/menus/credits.gd
+++ source/scripts/menus/credits.gd
@@ -2,8 +2,15 @@ extends Control
 
 var scrollSpeed:float = 100.0;
 
+func _ready() -> void:
+	MusicEngine.stopMusic();
+	MusicEngine.loadSong("bopeeboMoonlite");
+
 func _process(delta: float) -> void:
 	$credits.position.y -= delta * scrollSpeed;
 	
-	if $credits.position.y <= -2309:
+	#if $credits.position.y <= -2309:
+	if Input.is_action_just_pressed("uiEXIT"):
+		MusicEngine.stopMusic();
+		MusicEngine.loadSong("titleScreen");
 		get_tree().change_scene_to_file("res://source/scenes/menus/mainMenu.tscn");
