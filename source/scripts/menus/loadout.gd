7e8cd17fa0d2648633608103577c04d319a37a59
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-06 18:38:45 -0400

    final loadout menu polish

--- source/scripts/menus/loadout.gd
+++ source/scripts/menus/loadout.gd
@@ -10,10 +10,12 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	$weaponIcon/text.text = Stats.weapon;
 	if Input.is_action_just_pressed("uiLEFT"):
+		$AudioStreamPlayer.play();
 		curWeapon = curWeapon - 1;
 		if curWeapon == 0:
 			curWeapon = maxWeapon;
 	if Input.is_action_just_pressed("uiRIGHT"):
+		$AudioStreamPlayer.play();
 		curWeapon = curWeapon + 1;
 		if curWeapon > maxWeapon:
 			curWeapon = 1;
@@ -21,7 +23,8 @@ func _process(_delta: float) -> void:
 		$Arrow.modulate = Color("00ffff");
 	else:
 		$Arrow.modulate = Color("ffffff");
-	if Input.is_action_just_pressed("uiRIGHT"):
+		
+	if Input.is_action_pressed("uiRIGHT"):
 		$Arrow2.modulate = Color("00ffff");
 	else:
 		$Arrow2.modulate = Color("ffffff");
@@ -32,7 +35,19 @@ func _process(_delta: float) -> void:
 	match curWeapon:
 		1:
 			Stats.weapon = "Dark Sword";
+			Stats.weaponType = "Sword"; # TODO: make this read from a script rather than just setting the value here.
 		2:
 			Stats.weapon = "Hammer of Zoomin'";
+			Stats.weaponType = "Hammer";
 		3:
 			Stats.weapon = "Ground Pound";
+			Stats.weaponType = "Ground Pound";
+			
+	match Stats.weaponType:
+		"Sword":
+			$weaponIcon/sprite.texture = load("res://assets/shared/images/hud/weaponIcons/sword.png");
+		"Hammer":
+			$weaponIcon/sprite.texture = load("res://assets/shared/images/hud/weaponIcons/hammer.png");
+		"Ground Pound":
+			$weaponIcon/sprite.texture = load("res://assets/shared/images/hud/weaponIcons/groundPound.png");
+		
