64a0aa7df16cfab9922ee5874996eb055012aaba
TheSearcher213 <funnylookingcat5@gmail.com>
2026-04-07 16:51:04 -0400

    fixed the hammer not existing lol

---
+++ source/scripts/items/weapons/Rebounder.gd
@@ -0,0 +1,17 @@
+extends Weapon;
+
+var bonkSpeed:float = 850;
+
+func _ready():
+	baseDamage = 100;
+	attackCooldown = 0.1;
+	baseAttackSound = load("res://assets/shared/sounds/items/weapons/hammerOfZoomin.ogg");
+	exAttackSound = baseAttackSound;
+	affectsVelocity = true;
+	velocityTime = 3.5;
+	weaponType = "Hammer";
+
+func baseAttack():
+	velocityX = -bonkSpeed;
+	velocityY = -bonkSpeed;
+#Note to E-DubzNG to change the name soon. (delete when changed) - A Skeleton you know.
