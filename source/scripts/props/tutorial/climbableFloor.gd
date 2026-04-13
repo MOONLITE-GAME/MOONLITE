cf16142b2f9974ccc5041da21b70a50d2b4b2e6f
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-13 16:18:07 -0400

    lol i forgot to commit this yesterday

---
+++ source/scripts/props/tutorial/climbableFloor.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D;
+
+
+func _on_edge_left_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+func _on_edge_left_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+
+func _on_edge_right_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+func _on_edge_right_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
