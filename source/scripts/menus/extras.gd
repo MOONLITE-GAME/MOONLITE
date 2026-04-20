4a6185d32e0573e8ea0a8bf7c1e420a94603510e
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-19 23:26:41 -0400

    extras menu revamp

--- source/scripts/menus/extras.gd
+++ source/scripts/menus/extras.gd
@@ -1,8 +1,12 @@
 extends Control;
 
+var canVisitOST:bool = true;
+var curButton:int = 1;
+
 func _ready() -> void:
 	if OS.get_name() == "Web":
-		$ost.visible = false;
+		canVisitOST = false;
+	$credits.texture = load("res://assets/images/menus/extras/creditsSelect.png");
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://source/scenes/menus/credits.tscn");
@@ -13,3 +17,43 @@ func _on_ost_pressed() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("uiEXIT"):
 		get_tree().change_scene_to_file("res://source/scenes/menus/mainMenu.tscn");
+		
+	if Input.is_action_just_pressed("uiUP"):
+		$AudioStreamPlayer.play();
+		match curButton: 
+			1:
+				$credits.texture = load("res://assets/images/menus/extras/creditsNormal.png");
+				$ost.texture = load("res://assets/images/menus/extras/ostSelect.png");
+				$image.texture = load("res://assets/images/menus/extras/moonliteOST.png");
+				$image.position = Vector2(517.0, 191.0);
+				curButton = 2;
+			2:
+				$credits.texture = load("res://assets/images/menus/extras/creditsSelect.png");
+				$ost.texture = load("res://assets/images/menus/extras/ostNormal.png");
+				$image.texture = load("res://assets/images/menus/extras/credits.png");
+				$image.position = Vector2(517.0, 327.0);
+				curButton = 1;
+	if Input.is_action_just_pressed("uiDOWN"):
+		$AudioStreamPlayer.play();
+		match curButton: 
+			1:
+				$credits.texture = load("res://assets/images/menus/extras/creditsNormal.png");
+				$ost.texture = load("res://assets/images/menus/extras/ostSelect.png");
+				$image.texture = load("res://assets/images/menus/extras/moonliteOST.png");
+				$image.position = Vector2(517.0, 191.0);
+				curButton = 2;
+			2:
+				$credits.texture = load("res://assets/images/menus/extras/creditsSelect.png");
+				$ost.texture = load("res://assets/images/menus/extras/ostNormal.png");
+				$image.texture = load("res://assets/images/menus/extras/credits.png");
+				$image.position = Vector2(517.0, 327.0);
+				curButton = 1;
+	if Input.is_action_just_pressed("uiSELECT"):
+		match curButton:
+			1:
+				_on_credits_pressed();
+			2:
+				if canVisitOST:
+					_on_ost_pressed();
+				else:
+					pass;
