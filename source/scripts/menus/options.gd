2de0be65dd7603bc411da5cfa46005551632ef01
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-21 14:50:33 -0400

    options buttons texture fix

--- source/scripts/menus/options.gd
+++ source/scripts/menus/options.gd
@@ -8,21 +8,41 @@ func _input(event: InputEvent) -> void:
 		
 	match page:
 		"Video":
+			$video.texture_normal = load("res://assets/images/menus/options/buttonsPressed/video.png");
+			$audio.texture_normal = load("res://assets/images/menus/options/buttonsNormal/audio.png");
+			$controls.texture_normal = load("res://assets/images/menus/options/buttonsNormal/controls.png");
+			$other.texture_normal = load("res://assets/images/menus/options/buttonsNormal/other.png");
+			$exit.texture_normal = load("res://assets/images/menus/options/buttonsNormal/exit.png");
 			$"Video Settings".visible = true;
 			$"Audio Settings".visible = false;
 			$"Control Settings".visible = false;
 			$"Other Settings".visible = false;
 		"Audio":
+			$video.texture_normal = load("res://assets/images/menus/options/buttonsNormal/video.png");
+			$audio.texture_normal = load("res://assets/images/menus/options/buttonsPressed/audio.png");
+			$controls.texture_normal = load("res://assets/images/menus/options/buttonsNormal/controls.png");
+			$other.texture_normal = load("res://assets/images/menus/options/buttonsNormal/other.png");
+			$exit.texture_normal = load("res://assets/images/menus/options/buttonsNormal/exit.png");
 			$"Video Settings".visible = false;
 			$"Audio Settings".visible = true;
 			$"Control Settings".visible = false;
 			$"Other Settings".visible = false;
 		"Controls":
+			$video.texture_normal = load("res://assets/images/menus/options/buttonsNormal/video.png");
+			$audio.texture_normal = load("res://assets/images/menus/options/buttonsNormal/audio.png");
+			$controls.texture_normal = load("res://assets/images/menus/options/buttonsPressed/controls.png");
+			$other.texture_normal = load("res://assets/images/menus/options/buttonsNormal/other.png");
+			$exit.texture_normal = load("res://assets/images/menus/options/buttonsNormal/exit.png");
 			$"Video Settings".visible = false;
 			$"Audio Settings".visible = false;
 			$"Control Settings".visible = true;
 			$"Other Settings".visible = false;
 		"Other":
+			$video.texture_normal = load("res://assets/images/menus/options/buttonsNormal/video.png");
+			$audio.texture_normal = load("res://assets/images/menus/options/buttonsNormal/audio.png");
+			$controls.texture_normal = load("res://assets/images/menus/options/buttonsNormal/controls.png");
+			$other.texture_normal = load("res://assets/images/menus/options/buttonsPressed/other.png");
+			$exit.texture_normal = load("res://assets/images/menus/options/buttonsNormal/exit.png");
 			$"Video Settings".visible = false;
 			$"Audio Settings".visible = false;
 			$"Control Settings".visible = false;
