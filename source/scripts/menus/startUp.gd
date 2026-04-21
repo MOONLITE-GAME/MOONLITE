648ea27cd51e29b5139b0ba2b2491e2fb04aa426
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-04-21 14:33:24 -0400

    intro video skip

--- source/scripts/menus/startUp.gd
+++ source/scripts/menus/startUp.gd
@@ -2,6 +2,7 @@ extends Control
 
 var waitTime:float = 2.0;
 var canContinue:bool = false;
+var watchingMovie:bool = false;
 
 func _ready() -> void:
 	$AnyButton.visible = false;
@@ -12,6 +13,9 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("uiSELECT") and canContinue:
 		$VideoStreamPlayer.play();
-		
+		watchingMovie = true;
+	if event.is_action_pressed("SkipCutscene") and watchingMovie:
+		_on_video_stream_player_finished();
+	
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://source/scenes/menus/titleScreen.tscn");
