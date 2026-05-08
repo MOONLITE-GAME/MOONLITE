2e520b61b86363f427c0f84eda1018592d9f1f5e
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-07 21:24:41 -0400

    even MORE adjustments

--- source/scripts/levels/bridge/tutorial.gd
+++ source/scripts/levels/bridge/tutorial.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	print("Playing as character: " + Stats.character);
 	MusicEngine.stopMusic();
 	MusicEngine.loadSong("tutorial");
+	Stats.health = Stats.maxHealth;
 	Camera.limit_left = -1150;
 	Camera.limit_bottom = 1080;
 	
