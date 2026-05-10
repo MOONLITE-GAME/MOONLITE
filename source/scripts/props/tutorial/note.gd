e0593a85f1953a6899897dcf46eb2c0a571c1433
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-09 21:29:19 -0400

    funni ui by silly guy

--- source/scripts/props/tutorial/note.gd
+++ source/scripts/props/tutorial/note.gd
@@ -3,19 +3,28 @@ extends Area2D
 @export var dialogue:String;
 
 var inRadius:bool = false;
+var showingIMG:bool = false;
 
 func _process(_delta: float) -> void:
 	if Stats.mobile:
-		if Stats.attacking and Input.is_action_just_pressed("BasicAttack") and inRadius and not Stats.inDialogue:
+		if Stats.attacking and Input.is_action_just_pressed("Interact") and inRadius and not Stats.inDialogue:
+			showingIMG = true;
 			Stats.inDialogue = true;
-			Dialogic.start(dialogue);
+			#Dialogic.start(dialogue);
 	else:
-		if !Stats.attacking and Input.is_action_just_pressed("BasicAttack") and inRadius and not Stats.inDialogue:
+		if !Stats.attacking and Input.is_action_just_pressed("Interact") and inRadius and not Stats.inDialogue:
+			showingIMG = true;
 			Stats.inDialogue = true;
-			Dialogic.start(dialogue);
-		if Dialogic.VAR.isDone == true and Stats.inDialogue:
-			Dialogic.VAR.isDone = false;
-			Stats.inDialogue = false;
+			#Dialogic.start(dialogue);
+		#if Dialogic.VAR.isDone == true and Stats.inDialogue:
+			#Dialogic.VAR.isDone = false;
+			#Stats.inDialogue = false;
+	if Input.is_action_just_pressed("uiEXIT"):
+		showingIMG = false;
+		await get_tree().create_timer(0.5).timeout
+		Stats.inDialogue = false;
+		
+	$CanvasLayer.visible = showingIMG;
 
 
 func _on_body_entered(body: Node2D) -> void:
