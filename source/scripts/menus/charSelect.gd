5ed408ea82e91b228fc8af209dfd0ed92531b995
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-14 19:05:41 -0400

    finish character select for now

--- source/scripts/menus/charSelect.gd
+++ source/scripts/menus/charSelect.gd
@@ -2,3 +2,11 @@ extends Control
 
 func _ready() -> void:
 	MusicEngine.stopMusic();
+	updateChar();
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("uiSELECT"):
+		get_tree().change_scene_to_file("res://source/scenes/menus/loadout.tscn");
+
+func updateChar():
+	$ui/characterName.text = Stats.character;
