8d2ec6c89f7892cbb8f4e23b9915213c0d701d7e
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-14 19:22:52 -0400

    fix change_scene_to_file calls

--- source/scripts/menus/charSelect.gd
+++ source/scripts/menus/charSelect.gd
@@ -4,9 +4,12 @@ func _ready() -> void:
 	MusicEngine.stopMusic();
 	updateChar();
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("uiSELECT"):
 		get_tree().change_scene_to_file("res://source/scenes/menus/loadout.tscn");
+	if Input.is_action_just_pressed("uiEXIT"):
+		get_tree().change_scene_to_file("res://source/scenes/menus/mainMenu.tscn");
+		MusicEngine.loadSong("titleScreen");
 
 func updateChar():
 	$ui/characterName.text = Stats.character;
