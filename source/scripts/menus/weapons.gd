67babfc8fe1342f71d32cfb712dc5649c4259340
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-20 16:45:26 -0400

    glowww

--- source/scripts/menus/weapons.gd
+++ source/scripts/menus/weapons.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var curButton:int = 1;
+
 func _ready() -> void:
 	# TODO: Make this read from json to avoid hardcoding!!!
 	if Stats.character == "moonlite":
@@ -7,3 +9,19 @@ func _ready() -> void:
 		$character.texture = load("res://assets/images/menus/loadout/characters/moonlite.png");
 		$uiBack.texture = load("res://assets/images/menus/loadout/themes/back.png");
 		$start.texture = load("res://assets/images/menus/loadout/themes/start.png");
+
+func _process(delta: float) -> void:
+	match curButton:
+		1:
+			$cardGlow.visible = true;
+			$startGlow.visible = false;
+		2:
+			$cardGlow.visible = false;
+			$startGlow.visible = true;
+			
+	if Input.is_action_just_pressed("uiDOWN") or Input.is_action_just_pressed("uiUP"):
+		match curButton:
+			1:
+				curButton = 2;
+			2:
+				curButton = 1;
