f94d3aedc295d8b50155117ec033cc84ca081745
E-Dubz <86075067+EDubzNG@users.noreply.github.com>
2026-05-20 16:40:29 -0400

    more bullshit!!!

--- source/scripts/menus/weapons.gd
+++ source/scripts/menus/weapons.gd
@@ -4,3 +4,6 @@ func _ready() -> void:
 	# TODO: Make this read from json to avoid hardcoding!!!
 	if Stats.character == "moonlite":
 		$bg.color = Color("#A800FF");
+		$character.texture = load("res://assets/images/menus/loadout/characters/moonlite.png");
+		$uiBack.texture = load("res://assets/images/menus/loadout/themes/back.png");
+		$start.texture = load("res://assets/images/menus/loadout/themes/start.png");
